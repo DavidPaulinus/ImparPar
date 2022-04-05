e94fd0e5f54027c978d6633c89327d03a40bb7f8
Davi Paulino <davipm@hotmail.com>
2022-04-05 15:29:04 -0300

    Testando a estrutura

--- src/DesafioImparPar/SistemaImparPar.java
+++ src/DesafioImparPar/SistemaImparPar.java
@@ -1,10 +1,27 @@
 package DesafioImparPar;
 
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
 public class SistemaImparPar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
+		
+		int num;
+		char letra;
+		
+		System.out.print("Impar ou par? ");
+		letra=sc.next().charAt(0);
+		System.out.print("Manda o número que você quer usar: ");
+		num=sc.nextInt();
+		
+		System.out.println(letra);
+		
+		sc.close();
 	}
 
 }
